a70940799ea2b01be9803b66550489ee7b6a23d7
ivmerk <ivan.e.merkulov@gmail.com>
2021-04-26 07:48:29 +0300

    ceasarciphercracker.cpp finished

--- ceasarciphercracker.cpp
+++ ceasarciphercracker.cpp
@@ -18,11 +18,138 @@ answer:
 WHO CARES OF 1 THOUGH IT MAKES 2 */
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <string>
 using namespace std;
+const int KEYMIN = 1;
+const int KEYMAX = 25;
+bool checkTheWord(string checkingWord) //checking words from dictionarry
+{
+    //cout << "\nstartingchecking\n";
+    string wordFromFile;
+    ifstream in("./words.txt");
+    if (in.is_open())
+    {
+        while (getline(in, wordFromFile))
+        {
+            if (checkingWord == wordFromFile)
+            {
+                in.close();
+               // cout << "\ntrue\n";
+                return true;
+            }
+        }
+    }
+    in.close();
+   // cout << "\nfalse\n";
+    return false;
+}
+string decodWordLow (int key, string incomingWord)
+{
+    //cout << "\n" << incomingWord;
+    string outWord;
+    outWord = incomingWord;
+    for (int u = 0; u < outWord.size(); u++)
+        {   
+            if (key <= (outWord[u] - 'A'))
+            {
+                outWord[u] -= key;
+            } else {
+                outWord[u] += 'Z' - 'A' - key + 1;
+            }
+            outWord[u] += 'a' - 'A';
+        }
+    return outWord;
+}
+string decodWordUpper (int key, string incomingWord)
+{
+    string outWord;
+    outWord = incomingWord;
+    for (int u = 0; u < outWord.size(); u++)
+        {   
+            if (key <= (outWord[u] - 'A'))
+            {
+                outWord[u] -= key;
+            } else {
+                outWord[u] += 'Z' - 'A' - key + 1;
+            }
+        }
+    return outWord;
+}
+int checkingTheKey(vector <string> inputedWord)
+{
+    int key = KEYMIN;
+    bool keyReceived = false;
+    string wordBuffer;
+    while (!(keyReceived))
+    {
+        for (key = KEYMIN; key < KEYMAX; key++)
+        {
+            //cout << "\n" << key;
+            int k = 0;
+            do {
+            wordBuffer = inputedWord[k++];
+            wordBuffer = decodWordLow(key, wordBuffer);
+            wordBuffer.push_back('\r');
+            if (!checkTheWord(wordBuffer))
+            {
+                keyReceived = false;
+                break;
+            } else {
+                keyReceived = true;
+                //cout << wordBuffer;
+            }
+            } while (k < 3); // if key ok 3 words have to be in dict
+            if (keyReceived)
+            {
+                break;
+            }    
+        }
+    }
+
+    return key;
+}
 int main(int argc, char **argv)
 {
+    //string test = "XIP";
+    //int y = 1;
+    //cout << decodWord(y,test);
     int number_of_string;
-    // vector
-
+    vector <string> inputedWords;
+    vector <string> firstWordOfAnswer;
+    vector <string> secondWordOfAnswer;
+    vector <string> firdWordOfAnswer;
+    vector <int> keyResult;
+    cout << "input data:" << "\n";
+    cin >> number_of_string;
+    string lineBuffer, wordBuffer;
+    cin.ignore();
+    for (int i = 0; i < number_of_string; i++)
+    {
+        getline(cin, lineBuffer); // reading the line of input message
+        wordBuffer.erase();
+        int k = 0; // deviding string by words and putting to inputedWords vector
+        do {
+            if ((lineBuffer[k] != ' ') && (lineBuffer [k] != '\0'))
+            {
+                wordBuffer.push_back(lineBuffer[k++]);
+            } else {
+                k++; 
+                inputedWords.push_back(wordBuffer);
+                wordBuffer.erase();
+            }
+        } while (k <= lineBuffer.size());
+        keyResult.push_back(checkingTheKey(inputedWords));// send vestor to checking the key
+        firstWordOfAnswer.push_back(decodWordUpper(keyResult[i], inputedWords[0]));
+        secondWordOfAnswer.push_back(decodWordUpper(keyResult[i], inputedWords[1]));
+        firdWordOfAnswer.push_back(decodWordUpper(keyResult[i], inputedWords[2]));
+        inputedWords.clear(); 
+    } 
+    cout << "\nanswer:\n";
+    for (int i = 0; i < number_of_string; i++)
+    {
+        cout <<firstWordOfAnswer[i] << " " << secondWordOfAnswer[i] << " " << firdWordOfAnswer[i] << " " << keyResult[i] << " ";
+    }
+    cout << endl;
     return 0;
 }
