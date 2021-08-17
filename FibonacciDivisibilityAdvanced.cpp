bd93532e522142e9ad30254ef599c1717aa7563a
ivmerk <ivan.e.merkulov@gmail.com>
2021-08-17 22:07:38 +0300

    Levenshtein Distance starting

--- FibonacciDivisibilityAdvanced.cpp
+++ FibonacciDivisibilityAdvanced.cpp
@@ -17,3 +17,54 @@ input data:
 answer:
 1620 282
 Values will not exceed 2000000. */
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+int main()
+{
+    int data;
+    int fibNumber = 3;
+    int mdata;
+    int fibvalueminus = 1;
+    int tmp;
+    int fibvalue = 2;
+    vector<int> result;
+    string datastring;
+    cout << "\nввести число\n";
+    cin >> data;
+    cin.ignore();
+    getline(cin, datastring);
+    for (int i = 0; i < data; i++)
+    {
+        tmp = 0;
+        while ((datastring[0] != ' ') && (datastring[0] != '\n') && (datastring[0] != '\0'))
+        {
+
+            tmp = tmp * 10 + (datastring[0] - '0');
+            datastring.erase(datastring.begin());
+        }
+        datastring.erase(datastring.begin());
+        mdata = tmp;
+        fibNumber = 3;
+        fibvalueminus = 1;
+        fibvalue = 2;
+        while (((fibvalue + fibvalueminus) % mdata))
+
+        {
+            tmp = (fibvalue + fibvalueminus) % mdata;
+            fibvalueminus = fibvalue;
+            fibvalue = tmp;
+            fibNumber++;
+        }
+        result.push_back(++fibNumber);
+    }
+    cout << "\n";
+    while (result.size())
+    {
+
+        cout << result[0] << " ";
+        result.erase(result.begin());
+    }
+    return 0;
+}
