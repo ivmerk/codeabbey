8e5828d007943b8be2a405b8937bba807bea76cc
ivmerk <ivan.e.merkulov@gmail.com>
2021-08-29 22:58:08 +0300

    maze on the way

--- MazePathfinder.cpp
+++ MazePathfinder.cpp
@@ -45,7 +45,18 @@ answer:
 #include <vector>
 #include <string>
 using namespace std;
-vector<char> findTheWay(int widthStart, int highStart, int widthOfTheField, int highOfTheField, bool field[], vector<char> result);
+struct node
+{
+    //   int x;
+    //   int y;
+    string steps;
+    bool space;
+};
+vector<char> findTheWay(int widthStart, int highStart, int widthOfTheField, int highOfTheField, node *field, vector<char> result);
+vector<char> goUp(int widthStart, int highStart, int widthOfTheField, int highOfTheField, node field[], vector<char> result);
+vector<char> goLeft(int widthStart, int highStart, int widthOfTheField, int highOfTheField, node field[], vector<char> result);
+vector<char> goDown(int widthStart, int highStart, int widthOfTheField, int highOfTheField, node field[], vector<char> result);
+vector<char> goRight(int widthStart, int highStart, int widthOfTheField, int highOfTheField, node field[], vector<char> result);
 void vectorOptimization(vector<char> &result);
 int main(int argc, char *argv[])
 {
@@ -54,17 +65,21 @@ int main(int argc, char *argv[])
     vector<char> result;
     cout << "\ninput data:\n";
     cin >> widthOfTheField >> highOfTheField;
-    bool *field = new bool[widthOfTheField * highOfTheField]();
+    node *field = new node[widthOfTheField * highOfTheField]();
     cin.ignore();
     for (int i = 0; i < highOfTheField; i++)
     {
         getline(cin, datastring);
         for (int j = 0; j < widthOfTheField; j++)
         {
-            field[i * widthOfTheField + j] = datastring[j] - '0';
+            field[i * widthOfTheField + j].space = datastring[j] - '0';
+            field[i * widthOfTheField + j].steps.clear();
         }
     }
 
+    bool y = field[0].space;
+    bool u = field[13].space;
+    string df = field[0].steps;
     /*for (int i = 0; i < highOfTheField; i++)
     {
         cout << "\n";
@@ -78,77 +93,251 @@ int main(int argc, char *argv[])
     cout << "\n";
     return 0;
 }
-vector<char> findTheWay(int widthStart, int highStart, int widthOfTheField, int highOfTheField, bool field[], vector<char> result)
+vector<char> findTheWay(int widthStart, int highStart, int widthOfTheField, int highOfTheField, node *field, vector<char> result)
 {
-    if (widthStart || highStart)
+    while (!field[0].steps.size())
     {
         vector<char> newResult;
-        if (((highStart - 1) >= 0) && (field[widthOfTheField * (highStart - 1) + widthStart]))
+        if (((highStart - 1) >= 0) && (field[widthOfTheField * (highStart - 1) + widthStart].space) && (!field[widthOfTheField * (highStart - 1) + widthStart].steps.size()))
         {
-            bool *newField = new bool[widthOfTheField * highOfTheField]();
-            field[widthOfTheField * (highStart) + widthStart] = false;
-            field[widthOfTheField * (highStart - 1) + widthStart] = false;
-            newField = field;
-            newField[widthOfTheField * (highStart) + widthStart] = false;
-            result.push_back('U');
-            newResult = findTheWay(widthStart, highStart - 1, widthOfTheField, highOfTheField, newField, result);
-            delete[] newField;
+            field[widthOfTheField * (highStart - 1) + widthStart].steps = field[widthOfTheField * highStart + widthStart].steps;
+            field[widthOfTheField * (highStart - 1) + widthStart].steps.push_back('U');
+            newResult = goUp(widthStart, highStart - 1, widthOfTheField, highOfTheField, field, newResult);
             return newResult;
         }
+        if (((highStart + 1) < highOfTheField) && (field[widthOfTheField * (highStart + 1) + widthStart].space) && (!field[widthOfTheField * (highStart + 1) + widthStart].steps.size()))
+        {
+            field[widthOfTheField * (highStart + 1) + widthStart].steps = field[widthOfTheField * highStart + widthStart].steps;
+            field[widthOfTheField * (highStart + 1) + widthStart].steps.push_back('D');
+            newResult = goDown(widthStart, highStart + 1, widthOfTheField, highOfTheField, field, newResult);
+            return newResult;
+        }
+        if (((widthStart - 1) >= 0) && (field[widthOfTheField * highStart + widthStart - 1]))
+        {
+            newResult.push_back('L');
+            newResult = goLeft(widthStart - 1, highStart, widthOfTheField, highOfTheField, field, newResult);
+            return newResult;
+        }
+        if (((widthStart + 1) < widthOfTheField) &&
+            (field[widthOfTheField * highStart + widthStart + 1]))
+        {
+            newResult.push_back('R');
+            newResult = goRight(widthStart + 1, highStart, widthOfTheField, highOfTheField, field, newResult);
+            return newResult;
+        }
+
         else
         {
-            if (((highStart + 1) < highOfTheField) &&
-                (field[widthOfTheField * (highStart + 1) + widthStart]))
-            {
-                bool *newField = new bool[widthOfTheField * highOfTheField]();
-                field[widthOfTheField * (highStart) + widthStart] = false;
-                field[widthOfTheField * (highStart + 1) + widthStart] = false;
-                newField = field;
-                newField[widthOfTheField * (highStart) + widthStart] = false;
-                result.push_back('D');
-                result = findTheWay(widthStart, highStart + 1, widthOfTheField, highOfTheField, newField, result);
-                delete[] newField;
-                return result;
-            }
-            else
-            {
-                if (((widthStart - 1) >= 0) && (field[widthOfTheField * highStart + widthStart - 1]))
-                {
-                    bool *newField = new bool[widthOfTheField * highOfTheField]();
-                    field[widthOfTheField * (highStart) + widthStart] = false;
-                    field[widthOfTheField * (highStart) + widthStart - 1] = false;
-                    newField = field;
-                    newField[widthOfTheField * (highStart) + widthStart] = false;
-                    result.push_back('L');
-                    result = findTheWay(widthStart - 1, highStart, widthOfTheField, highOfTheField, field, result);
-                    delete[] newField;
-                    return result;
-                }
-                else
-                {
-
-                    if (((widthStart + 1) < widthOfTheField) &&
-                        (field[widthOfTheField * highStart + widthStart + 1]))
-                    {
-                        bool *newField = new bool[widthOfTheField * highOfTheField]();
-                        field[widthOfTheField * (highStart) + widthStart] = false;
-                        field[widthOfTheField * (highStart) + widthStart + 1] = false;
-                        newField = field;
-                        newField[widthOfTheField * (highStart) + widthStart] = false;
-                        result.push_back('R');
-                        result = findTheWay(widthStart + 1, highStart, widthOfTheField, highOfTheField, field, result);
-                        delete[] newField;
-                        return result;
-                    }
-
-                    else
-                    {
-                        result.push_back('T');
-                        return result;
-                    }
-                }
-            }
+            newResult.push_back('T');
+            return newResult;
         }
     }
     return result;
 }
+vector<char> goUp(int widthStart, int highStart, int widthOfTheField, int highOfTheField, bool field[], vector<char> result)
+{
+    int high = highStart;
+    int width = widthStart;
+    vector<char> newResult = result;
+    bool *newField = new bool[widthOfTheField * highOfTheField]();
+    newField = field;
+    while (((high >= 0) && (high - 1)))
+    {
+
+        if ((width > (widthOfTheField * (high))) && (newField[widthOfTheField * high + width - 1]))
+        {
+            newResult.push_back('L');
+            width--;
+            vector<char> tempResult = goLeft(width, high, widthOfTheField, highOfTheField, newField, newResult);
+            if (tempResult.back() == 'T')
+            {
+                tempResult.clear();
+                tempResult.shrink_to_fit();
+            }
+            else
+            {
+                delete[] newField;
+                return tempResult;
+            }
+        }
+        if ((width < (widthOfTheField * (high + 1) - 1)) && (newField[widthOfTheField * high + width + 1]))
+        {
+            newResult.push_back('R');
+            width++;
+            vector<char> tempResult = goRight(width, high, widthOfTheField, highOfTheField, newField, newResult);
+            if (tempResult.back() == 'T')
+            {
+                tempResult.clear();
+                tempResult.shrink_to_fit();
+            }
+            else
+            {
+                delete[] newField;
+                return tempResult;
+            }
+        }
+        newResult.push_back('U');
+        high--;
+        if ((high == 0) && (width == 0))
+        {
+            delete[] newField;
+            return newResult;
+        }
+    }
+    delete[] newField;
+    newResult.push_back('T');
+    return newResult;
+}
+vector<char> goLeft(int widthStart, int highStart, int widthOfTheField, int highOfTheField, bool field[], vector<char> result)
+{
+    int high = highStart;
+    int width = widthStart;
+    vector<char> newResult = result;
+    bool *newField = new bool[widthOfTheField * highOfTheField]();
+    newField = field;
+    while (((width >= 1) && (width - 1)))
+    {
+
+        if ((high < (highOfTheField - 1)) && (newField[widthOfTheField * (high - 1) + width]))
+        {
+            newResult.push_back('U');
+            high--;
+            vector<char> tempResult = goUp(width, high, widthOfTheField, highOfTheField, newField, newResult);
+            if (tempResult.back() == 'T')
+            {
+                tempResult.clear();
+                tempResult.shrink_to_fit();
+            }
+            else
+            {
+                delete[] newField;
+                return tempResult;
+            }
+        }
+        if ((high) && (newField[widthOfTheField * (high + 1) + width]))
+        {
+            newResult.push_back('D');
+            high++;
+            vector<char> tempResult = goDown(width, high, widthOfTheField, highOfTheField, newField, newResult);
+            if (tempResult.back() == 'T')
+            {
+                tempResult.clear();
+                tempResult.shrink_to_fit();
+            }
+            else
+            {
+                delete[] newField;
+                return tempResult;
+            }
+        }
+        newResult.push_back('L');
+        width--;
+        if ((high == 0) && (width == 0))
+        {
+            delete[] newField;
+            return newResult;
+        }
+    }
+    delete[] newField;
+    newResult.push_back('T');
+    return newResult;
+}
+vector<char> goDown(int widthStart, int highStart, int widthOfTheField, int highOfTheField, bool field[], vector<char> result)
+{
+    int high = highStart;
+    int width = widthStart;
+    vector<char> newResult = result;
+    bool *newField = new bool[widthOfTheField * highOfTheField]();
+    newField = field;
+    while (((high < (highOfTheField - 1)) && (high + 1)))
+    {
+
+        if ((width > (widthOfTheField * (high))) && (newField[widthOfTheField * high + width - 1]))
+        {
+            newResult.push_back('L');
+            width--;
+            vector<char> tempResult = goLeft(width, high, widthOfTheField, highOfTheField, newField, newResult);
+            if (tempResult.back() == 'T')
+            {
+                tempResult.clear();
+                tempResult.shrink_to_fit();
+            }
+            else
+            {
+                delete[] newField;
+                return tempResult;
+            }
+        }
+        if ((width < (widthOfTheField * (high + 1) - 1)) && (newField[widthOfTheField * high + width + 1]))
+        {
+            newResult.push_back('R');
+            width++;
+            vector<char> tempResult = goRight(width, high, widthOfTheField, highOfTheField, newField, newResult);
+            if (tempResult.back() == 'T')
+            {
+                tempResult.clear();
+                tempResult.shrink_to_fit();
+            }
+            else
+            {
+                delete[] newField;
+                return tempResult;
+            }
+        }
+        newResult.push_back('D');
+        high++;
+    }
+    delete[] newField;
+    newResult.push_back('T');
+    return newResult;
+}
+vector<char> goRight(int widthStart, int highStart, int widthOfTheField, int highOfTheField, bool field[], vector<char> result)
+{
+    int high = highStart;
+    int width = widthStart;
+    vector<char> newResult = result;
+    bool *newField = new bool[widthOfTheField * highOfTheField]();
+    newField = field;
+    while (((width < widthOfTheField * (high + 1)) && (width + 1)))
+    {
+
+        if (high && (newField[widthOfTheField * (high - 1) + width]))
+        {
+            newResult.push_back('U');
+            high--;
+            vector<char> tempResult = goUp(width, high, widthOfTheField, highOfTheField, newField, newResult);
+            if (tempResult.back() == 'T')
+            {
+                tempResult.clear();
+                tempResult.shrink_to_fit();
+            }
+            else
+            {
+                delete[] newField;
+                return tempResult;
+            }
+        }
+        if ((high < (highOfTheField - 1)) && (newField[widthOfTheField * (high + 1) + width]))
+        {
+            newResult.push_back('D');
+            high++;
+            vector<char> tempResult = goDown(width, high, widthOfTheField, highOfTheField, newField, newResult);
+            if (tempResult.back() == 'T')
+            {
+                tempResult.clear();
+                tempResult.shrink_to_fit();
+            }
+            else
+            {
+                delete[] newField;
+                return tempResult;
+            }
+        }
+        newResult.push_back('R');
+        width++;
+    }
+    delete[] newField;
+    newResult.push_back('T');
+    return newResult;
+}
+* /
