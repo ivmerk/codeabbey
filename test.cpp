72df8b4363a478d69954ef6b7bff5b877b90133c
ivmerk <ivan.e.merkulov@gmail.com>
2021-09-09 20:44:09 +0300

    fibobrins starrted

--- test.cpp
+++ test.cpp
@@ -1,16 +1,20 @@
+#include <string>
 #include <iostream>
-#
 using namespace std;
-
-int main()
-
+bool solution(std::string const &str, std::string const &ending)
 {
-    int n = 16;
-    int result = 0;
-    while (n) // ...
-    {
-        result += n % 10;
-        n /= 10;
-    }
-    return result;
+    long long tmp = str.size() - ending.size();
+    std::string tmpstr = str;
+    tmpstr.erase(0, tmp);
+    cout << tmpstr;
+    return ((tmpstr == ending) || !ending.size());
+}
+int main()
+{
+    std::string str = "abcde";
+    std::string ending = "cde";
+    std::string tmp = str.substr(ending.size() - 1);
+    bool answer = solution(str, ending);
+    //cout << answer;
+    return 0;
 }
